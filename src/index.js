53731945a592fa47be69c237741ad06c859b148f
Francis Tayag <zyenapz@gmail.com>
2025-03-03 17:31:02 +0800

    Add basic JS proof of concept

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-const paragraph = document.createElement("p");
-paragraph.textContent = "Hello!";
-document.body.appendChild(paragraph);
+
+const btnHome = document.querySelector("#home");
+const btnMenu = document.querySelector("#menu");
+const btnAbout = document.querySelector("#about");
+
+btnHome.addEventListener("click", (e) => {
+    const content = document.querySelector("#content");
+    const paragraph = document.createElement("p");
+    // Clear
+    content.replaceChildren();
+
+    // Append
+    paragraph.textContent = "Home!";
+    content.appendChild(paragraph);
+});
+
+btnMenu.addEventListener("click", (e) => {
+    const content = document.querySelector("#content");
+    const paragraph = document.createElement("p");
+    // Clear
+    content.replaceChildren();
+
+    // Append
+    paragraph.textContent = "Menu!";
+    content.appendChild(paragraph);
+});
+
+btnAbout.addEventListener("click", (e) => {
+    const content = document.querySelector("#content");
+    const paragraph = document.createElement("p");
+    // Clear
+    content.replaceChildren();
+
+    // Append
+    paragraph.textContent = "About!";
+    content.appendChild(paragraph);
+});
