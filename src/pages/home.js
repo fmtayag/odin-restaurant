a6931184b834db8a2184378f3c42ac19d3833c9e
Francis Tayag <zyenapz@gmail.com>
2025-03-04 12:33:24 +0800

    Refactor homepage design

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,15 @@
 import '../styles/home.css';
+import backgroundImage from '../../assets/overlay.png';
 
 export default function createHome() {
     content.innerHTML = `
-    <div class="content home">
-        <h1 class="title">It's Time To Eat!</h1>
-        <p class="secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, eius! Dignissimos aut exercitationem quibusdam praesentium laboriosam aperiam ducimus corporis illum similique officia, earum esse consequuntur sint ipsum voluptatum, debitis ea.</p>
-        <button type="button" class="primary">See Our Menu</button>
+    <div class="home">
+        <div>
+            <h1 class="title">The Odin Steakhouse</h1>
+            <p class="secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, eius! Dignissimos aut exercitationem quibusdam praesentium laboriosam aperiam ducimus corporis illum similique officia, earum esse consequuntur sint ipsum voluptatum, debitis ea.</p>
+            <button type="button" class="primary">See Our Menu</button>
+        </div>
+
     </div>
     `;
 };
