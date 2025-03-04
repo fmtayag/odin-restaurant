e088a6e4fa611154f34fc5641439e984bf225b1e
Francis Tayag <zyenapz@gmail.com>
2025-03-04 11:26:29 +0800

    Style the homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,11 @@
+import '../styles/home.css';
+
 export default function createHome() {
     content.innerHTML = `
-    <div>
-        <h1>It's Time To Eat!</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, eius! Dignissimos aut exercitationem quibusdam praesentium laboriosam aperiam ducimus corporis illum similique officia, earum esse consequuntur sint ipsum voluptatum, debitis ea.</p>
-        <button type="button">Menu</button>
+    <div class="content home">
+        <h1 class="title">It's Time To Eat!</h1>
+        <p class="secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, eius! Dignissimos aut exercitationem quibusdam praesentium laboriosam aperiam ducimus corporis illum similique officia, earum esse consequuntur sint ipsum voluptatum, debitis ea.</p>
+        <button type="button" class="primary">See Our Menu</button>
     </div>
     `;
 };
