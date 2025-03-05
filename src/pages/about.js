7b2c241f91c457302a30f2c7a941e1225c534fed
Francis Tayag <zyenapz@gmail.com>
2025-03-05 11:00:55 +0800

    Add hero image, and expand content (About page)

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,22 @@
 import '../styles/about.css';
+import heroImage from '../../assets/hero-1.jpg';
 
 export default function createAbout() {
     content.innerHTML = `
     <div class="about">
         <h1 class="title">About Us</h1>
-        <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+        <div class="content">
+            <div class="hero"></div>
+            <h2>Only The Best Since 1066</h2>
+            <p class="secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <p class="secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.  </p>
+            <h2>Authentic Valhalla Cuisine</h2>
+            <p class="secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
         </div>
         
         <div class="contact">
             <ul class="location">
-                <li><h2 class="header name">Visit Us</h2></li>
+                <li><h2 class="name">Visit Us</h2></li>
                 <li class="subheader"><h3>Valhalla Branch</h3></li>
                 <li>123 Asgard Street, Valhalla</li>
                 <li class="subheader"><h3>Jotunheim Branch</h3></li>
@@ -20,7 +24,7 @@ export default function createAbout() {
                 <li>All branches open from 12nn to 9pm.</li>
             </ul>
             <ul class="details">
-                <li><h2 class="header name">Contact us</h2></li>
+                <li><h2 class="name">Contact us</h2></li>
                 <li>theodinsteakhouse@odinmail.com</li>
                 <li>(001) 1234567890</li>
             </ul>
