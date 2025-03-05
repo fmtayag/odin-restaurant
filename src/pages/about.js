209a16b25aa2141fad3a5a19543cfa1c980dd93e
Francis Tayag <zyenapz@gmail.com>
2025-03-05 15:43:17 +0800

    Replace lorem about us page with cheesy descriptions

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,11 +11,10 @@ export default function createAbout() {
                 Photo by KamranAydinov on Freepik.
             </a>
             </div>
-            <h2>Only The Best Since 1066</h2>
-            <p class="secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-            <p class="secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.  </p>
+            <h2>The Best Since 1066</h2>
+            <p class="secondary">Established in 1066, the Odin Steakhouse has had a long history of serving Kings, Queens, warriors, and wanderers alike. Ever since our forefathers arrived in the shores of Albion, the Odin Steakhouse has been a bastion for quality, and has served its patrons faithfully and unceasingly as the best steakhouse in all of Midgard. Through war and peace, our hearth has never grown cold, and our mead has never ceased flowing.</p>
             <h2>Authentic Valhalla Cuisine</h2>
-            <p class="secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <p class="secondary">Experience <em>the best</em> steakhouse in all of Midgard. Our ingredients are carefully vetted and curated, ensuring that only the finest will make it onto your table. Trained by Andhrímnir himself, our chefs are second to none, and will relentlessly guarantee that the food on your plate will be fit for a Norse god. That, is the Odin promise.</p>
         </div>
         
         <div class="info">
