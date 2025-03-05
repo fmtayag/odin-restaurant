7cf68494635f3ac0ec518e8f701ae9550943f3d4
Francis Tayag <zyenapz@gmail.com>
2025-03-05 15:18:25 +0800

    Replace main dish lorem with actual descriptions

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -11,27 +11,27 @@ export default function createMenu() {
                     <li class="food-item">
                         <div class="name">Odin's Ire</div>
                         <div class="price">$60</div>
-                        <div class="description">Lorem ipsum dolor sit amet adipisicing elit.</div>
+                        <div class="description">Pan-seared T-bone steak, basted with garlic and our trade-secret compound butter.</div>
                     </li>
                     <li class="food-item">
                         <div class="name">Thor's Hammer</div>
                         <div class="price">$30</div>
-                        <div class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                        <div class="description">Prime tomahawk steak, basted with garlic butter, and served with mashed potatoes.</div>
                     </li>
                     <li class="food-item">
                         <div class="name">Asgardian's Feast</div>
                         <div class="price">$20</div>
-                        <div class="description">Lorem ipsum dolor sit amet consectetur adipisicing.</div>
+                        <div class="description">Prime sirloin steak coated in melted butter made with freshly minced garlic.</div>
                     </li>
                     <li class="food-item">
                         <div class="name">Jötunn's Bounty</div>
                         <div class="price">$20</div>
-                        <div class="description">Lorem ipsum dolor sit amet adipisicing elit.</div>
+                        <div class="description">Prime flank steak marinated in butter, and garnished with lemon and rosemary.</div>
                     </li>
                     <li class="food-item">
                         <div class="name">Fenrir's Bite</div>
                         <div class="price">$15</div>
-                        <div class="description">Lorem ipsum dolor sit amet adipisicing elit.</div>
+                        <div class="description">Prime filet mignon garnished with rosemary, and pan-seared to perfection.</div>
                     </li>
                 </ul>
                 <h2 class="category">Sides</h2>
